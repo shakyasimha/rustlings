5c2c5ae843d822f677cd8f747f43bd4a043170e3
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2025-03-14 16:21:15 +0545

    stuck on move_semantics2

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -1,13 +1,24 @@
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
+    // let mut vec = vec;
 
-    vec.push(88);
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+    // vec.push(88);
 
     vec
 }
 
 fn main() {
     // You can optionally experiment here.
+    let mut vec0 = Vec::new(); 
+    let mut vec1 = fill_vec(&mut vec0);
+
+    println!("{} has length {} and content `{:?}`", "vec0", vec0.len(), vec0);
+
+    vec1.push(88);
+
+    println!("{} has length {} and content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
 #[cfg(test)]
@@ -18,9 +29,9 @@ mod tests {
     // fix the compiler error in the test.
     #[test]
     fn move_semantics2() {
-        let vec0 = vec![22, 44, 66];
+        let mut vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let mut vec1 = fill_vec($mut vec0);
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
